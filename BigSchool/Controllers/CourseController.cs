783da6fb6078635d9cc4248f98afe147ae2443f2
Minhne123 <fot123456qwe@gmail.com>
2021-07-16 09:01:53 +0700

    congminh

--- BigSchool/Controllers/CourseController.cs
+++ BigSchool/Controllers/CourseController.cs
@@ -128,5 +128,36 @@ namespace BigSchool.Controllers
             return RedirectToAction("Mine", "Courses");
         }
 
+        public ActionResult LectureIamGoing()
+        {
+            ApplicationUser currentUser =
+            System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>()
+            .FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+
+            var listFollwee = con.Followings.Where(p => p.FollowerId == currentUser.Id).ToList();
+
+            var listAttendances = con.Attendances.Where(p => p.Attendee ==
+
+            currentUser.Id).ToList();
+
+            var courses = new List<Course>();
+            foreach (var course in listAttendances)
+
+            {
+                foreach (var item in listFollwee)
+                {
+                    if (item.FolloweeId == course.Course.LectureId)
+                    {
+                        Course objCourse = course.Course;
+                        objCourse.LectureName =
+                        System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>()
+                        .FindById(objCourse.LectureId).Name;
+                        courses.Add(objCourse);
+                    }
+                }
+            }
+            return View(courses);
+        }
+
     }
 }
